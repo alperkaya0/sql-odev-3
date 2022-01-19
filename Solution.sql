93906bf1a50013b635e34963405d1377eb07ac98
Alper Kaya <73350743+alperkaya0@users.noreply.github.com>
2022-01-19 17:53:36 +0300

    Create Solution.sql

---
+++ Solution.sql
@@ -0,0 +1,22 @@
+--SELECT * FROM country WHERE country LIKE 'A%a';
+--SELECT * FROM country WHERE country LIKE '%_____%n';
+--SELECT * FROM film WHERE title ILIKE '%T%T%T%T%';
+--SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
+
+ENGLISH:
+--Hello,
+
+--Query all columns of country table where country column's value starts with 'A' and ends with 'a'.
+--Query all columns of country table where country column's value consists of at least 6 chars and ends with 'n' character.
+--Query all columns of film table where title column's value includes at least 4 non-case sensitive 'T' character.
+--Query all columns of film table where title column's value starts with 'C' and length column's value bigger than 90 and rental_rate column's value equals to 2.99.
+
+TURKISH:
+--Merhabalar,
+
+--Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+--country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+--country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+--film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+--film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
